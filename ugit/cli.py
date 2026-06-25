80adb77f5244bf22a6d21cde7a0e9c31db06cba8
Hanin <smhanin2718@gmail.com>
2026-06-25 04:07:34 +0000

    Implement blob object hashing and storage

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,7 +1,9 @@
 import argparse
 import os
+import sys
 
 from . import data
+
 
 def main():
     args = parse_args()
@@ -11,14 +13,34 @@ def main():
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    commands = parser.add_subparsers(dest='command')
+    commands = parser.add_subparsers(dest="command")
     commands.required = True
 
-    init_parser = commands.add_parser('init')
+    init_parser = commands.add_parser("init")
     init_parser.set_defaults(func=init)
+
+    hash_object_parser = commands.add_parser("hash-object")
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument("file")
+
+    cat_file_parser = commands.add_parser("cat-file")
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument("object")
 
     return parser.parse_args()
 
+
 def init(args):
     data.init()
-    print(f'Initialized empty ugit repository in {os.getcwd()}/{data.GIT_DIR}')
+    print(f"Initialized empty ugit repository in {os.getcwd()}/{data.GIT_DIR}")
+
+
+def hash_object(args):
+    with open(args.file, "rb") as f:  # rb = binary mode, handles any file type
+        print(data.hash_object(f.read()))
+
+
+def cat_file(args):
+    sys.stdout.flush()
+    # write raw bytes directly to stdout buffer - handles binary files
+    sys.stdout.buffer.write(data.get_object(args.object, expected=None))
--- ugit/data.py
+++ ugit/data.py
@@ -1,7 +1,30 @@
+import hashlib
 import os
 
-GIT_DIR = '.ugit'
+GIT_DIR = ".ugit"
 
 
 def init():
     os.makedirs(GIT_DIR)
+    os.makedirs(f'{GIT_DIR}/objects')
+
+
+def hash_object(data, type_='blob'):
+    obj = type_.encode() + b'\x00' + data
+    oid = hashlib.sha1(obj).hexdigest()
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(obj)
+    return oid
+
+
+def get_object(oid, expected='blob'):
+    with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+        obj = f.read()
+
+    # returns: (b'blob', b'\x00', b'hello world')
+    type_, _, content = obj.parition(b'\x00')
+    type_ = type_.decode()
+
+    if expected is not None:
+        assert type_ == expected, f'Expected {expected}, got {type}'
+    return content
